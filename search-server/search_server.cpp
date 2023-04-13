d9ecb7dfec97fa4490a4f9aae30fc18af1becef5
huballs <ballshugo@yandex.ru>
2023-04-13 16:08:42 +0300

    modified:   search-server/search_server.cpp

--- search-server/search_server.cpp
+++ search-server/search_server.cpp
@@ -67,17 +67,18 @@ SearchServer::MatchDocument(const std::string& raw_query,
         }
     }
 
-    std::vector<std::string> matched_words;
+    std::set<std::string> matched_words;
     for (const std::string& word : query.plus_words) {
         if (word_to_document_freqs_.count(word) == 0) {
             continue;
         }
         if (word_to_document_freqs_.at(word).count(document_id)) {
-            matched_words.push_back(word);
+            matched_words.insert(word);
         }
     }
 
-    return {matched_words, documents_.at(document_id).status};
+    return {std::vector<std::string>(matched_words.begin(), matched_words.end()), 
+            documents_.at(document_id).status};
 }
 
 void SearchServer::AddDocument(int document_id, const std::string& document, DocumentStatus status,
